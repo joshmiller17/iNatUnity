a0d7b890d2e9f09e1a9ef53e009079d853e919c1
Josh Aaron Miller <miller.josh@husky.neu.edu>
2021-11-29 12:36:35 -0500

    Switch web requests to fully async

--- Assets/INatManager.cs
+++ Assets/INatManager.cs
@@ -15,11 +15,35 @@ namespace JoshAaronMiller.INaturalist
     /// </summary>
     public class INatManager : MonoBehaviour
     {
-        public static string BaseUrl = "https://api.inaturalist.org/v1/";
+        public static readonly string BaseUrl = "https://api.inaturalist.org/v1/";
+        static List<Observation> JsonToObservations(string jsonString) => ObsWebResult.CreateFromJson(jsonString).results;
+        static Observation JsonToObservation(string jsonString) => ObsWebResult.CreateFromJson(jsonString).results[0];
 
-        //TEST
+        static readonly float ServerSleepTime = 3; //be nice to server
+        static float timeSinceLastServerCall = float.MaxValue;
+
+
         void Start()
         {
+            Test();
+        }
+
+        private void Update()
+        {
+            if (timeSinceLastServerCall < ServerSleepTime)
+            {
+                timeSinceLastServerCall += Time.unscaledDeltaTime;
+            }
+        }
+
+        bool IsRateLimited()
+        {
+            return timeSinceLastServerCall < ServerSleepTime;
+        }
+
+        void Test()
+        {
+            Debug.Log("Running test");
             ObservationSearch os = new ObservationSearch();
             os.SetOrder(ObservationSearch.OrderBy.SpeciesGuess, ObservationSearch.SortOrder.Asc);
             os.SetQualityGrade(ObservationSearch.QualityGrade.NeedsId);
@@ -27,7 +51,12 @@ namespace JoshAaronMiller.INaturalist
             os.SetObservedOnDateLimits("2021-6-1", "2021-8-1");
             os.SetBooleanParameter(ObservationSearch.BooleanParameter.HasPhotos, true);
             os.SetBooleanParameter(ObservationSearch.BooleanParameter.IsPopular, true);
-            List<Observation> results = SearchObservations(os);
+            SearchObservations(os, TestCallback);
+        }
+
+        void TestCallback(List<Observation> results)
+        {
+            Debug.Log("Test callback");
             foreach (Observation r in results)
             {
                 foreach (string url in r.GetPhotoUrls(Observation.ImageSize.Large))
@@ -38,31 +67,31 @@ namespace JoshAaronMiller.INaturalist
         }
 
         /// <summary>
-        /// Performs a web request.
-        /// </summary>
-        /// <param name="request">The HttpWebRequest</param>
-        /// <returns>The JSON response in string format. To extract, use JsonUtility.FromJson<DataType>(jsonString)</returns>
-        public string DoWebRequest(HttpWebRequest request)
-        {
-            Debug.Log("[INatManager] Web request: " + request.RequestUri.ToString());
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            StreamReader reader = new StreamReader(response.GetResponseStream());
-            return reader.ReadToEnd();
-        }
-
-        /// <summary>
         /// Perform an asynchronous web request.
         /// </summary>
         /// <param name="request">The UnityWebRequest</param>
-        /// <param name="callback">A function to callback when the request is done which takes as input the JSON response as a string.</param>
-        public IEnumerator DoWebRequestAsync(UnityWebRequest request, Action<string> callback)
+        /// <param name="receiveRequest">The function which processes the server response (JSON encoded as string) and returns a response of type T.</param>
+        /// <param name="callback">A function to callback when the request is done which takes as input the processed response of type T.</param>
+        IEnumerator DoWebRequestAsync<T>(UnityWebRequest request, Func<string, T> receiveRequest, Action<T> callback)
         {
+            if (IsRateLimited())
+            {
+                yield return null;
+            }
+            Debug.Log("Sending web request: " + request.ToString());
             yield return request.SendWebRequest();
             while (!request.isDone)
                 yield return null;
             byte[] result = request.downloadHandler.data;
             string json = System.Text.Encoding.Default.GetString(result);
-            callback(json);
+
+            //DEBUG PRINT TODO REMOVE
+            string destination = Application.persistentDataPath + "/log.txt";
+            File.WriteAllText(destination, json);
+
+            T response;
+            response = receiveRequest(json);
+            callback(response);
         }
 
         // --- ANNOTATIONS ---
@@ -98,23 +127,25 @@ namespace JoshAaronMiller.INaturalist
         /// <summary>
         /// Given an array of IDs, returns corresponding observations 
         /// </summary>
-        /// <param name="id">The list of observation IDs to fetch</param>
-        /// <returns>A list of Observation JSON objects</returns>
-        public List<Observation> GetObservationsById(List<int> ids)
+        /// <param name="ids">The list of observation IDs to fetch</param>
+        /// <param name="callback">A function to callback when the request is done which takes as input the list of Observation objects found.</param>
+        public void GetObservationsById(List<int> ids, Action<List<Observation>> callback)
         {
             string idsAsStringList = string.Join(",", ids);
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(BaseUrl + "observations/" + idsAsStringList);
-            return ObsWebResult.CreateFromJson(DoWebRequest(request)).results;
+            UnityWebRequest request = UnityWebRequest.Get(BaseUrl + "observations/" + idsAsStringList);
+            StartCoroutine(DoWebRequestAsync(request, JsonToObservations, callback));
         }
 
+
         /// <summary>
-        /// Given an ID, returns the corresponding observation
+        /// Given an ID, returns corresponding observations
         /// </summary>
-        /// <param name="id">The list of observation IDs to fetch</param>
-        /// <returns>The Observation JSON object</returns>
-        public Observation GetObservationById(int id)
+        /// <param name="id">The observation ID to fetch</param>
+        /// <param name="callback">A function to callback when the request is done which takes as input the Observation object found.</param>
+        public void GetObservationById(List<int> id, Action<Observation> callback)
         {
-            return GetObservationsById(new List<int>() { id })[0];
+            UnityWebRequest request = UnityWebRequest.Get(BaseUrl + "observations/" + id.ToString());
+            StartCoroutine(DoWebRequestAsync(request, JsonToObservation, callback));
         }
 
 
@@ -131,21 +162,16 @@ namespace JoshAaronMiller.INaturalist
         //VoteObservation
         //UnvoteObservation
 
+
         /// <summary>
         /// Given an ObservationSearch object, returns a list of matching observations
         /// </summary>
         /// <param name="obsSearch">An ObservationSearch object holding the parameters of the search</param>
-        /// <returns></returns>
-        public List<Observation> SearchObservations(ObservationSearch obsSearch)
+        /// <param name="callback">A function to callback when the request is done which takes as input the list of Observation objects found.</param>
+        public void SearchObservations(ObservationSearch obsSearch, Action<List<Observation>> callback)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(BaseUrl + "observations/?" + obsSearch.ToUrlParameters());
-            
-            //TEST BLOCK
-            string test = DoWebRequest(request); //TEST
-            string destination = Application.persistentDataPath + "/log.txt";
-            File.WriteAllText(destination, test);
-
-            return ObsWebResult.CreateFromJson(test).results;
+            UnityWebRequest request = UnityWebRequest.Get(BaseUrl + "observations/?" + obsSearch.ToUrlParameters());
+            StartCoroutine(DoWebRequestAsync(request, JsonToObservations, callback));
         }
 
         //CreateObservation
